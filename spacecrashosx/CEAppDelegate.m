127787c5d24bad9080a0822416cfbd8b47afcd15
Chris Edson <christopher.edson@gmail.com>
2014-04-19 17:06:57 +0100

    Initial Commit

---
+++ spacecrashosx/CEAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CEAppDelegate.m
+//  spacecrashosx
+//
+//  Created by Chris Edson on 19/04/2014.
+//  Copyright (c) 2014 Chris Edson. All rights reserved.
+//
+
+#import "CEAppDelegate.h"
+
+@implementation CEAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
